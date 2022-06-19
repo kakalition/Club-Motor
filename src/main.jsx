f95932bf14b06ba0a25a2621abd6c02238d962f1
Kaka <kakalition@gmail.com>
2022-06-19 17:32:06 +0700

    Membuat routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HalamanAboutUs from './Halaman/About Us/HalamanAboutUs'
+import HalamanArtikel from './Halaman/Artikel/HalamanArtikel'
+import HalamanEvent from './Halaman/Event/HalamanEvent'
+import HalamanGalleryFoto from './Halaman/Gallery Foto/HalamanGalleryFoto'
+import HalamanKlienKamu from './Halaman/Klien Kami/HalamanKlienKami'
+import HalamanKontakKami from './Halaman/Kontak Kami/HalamanKontakKami'
+import HalamanProfile from './Halaman/Profile/HalamanProfile'
+import HalamanSignIn from './Halaman/Sign In/HalamanSignIn'
+import HalamanSignUp from './Halaman/Sign Up/HalamanSignUp'
 import HalamanUtama from './Halaman/Utama/HalamanUtama'
 import './index.css'
 
@@ -9,6 +18,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HalamanUtama />} />
+        <Route path='/about-us' element={<HalamanAboutUs />} />
+        <Route path='/artikel' element={<HalamanArtikel />} />
+        <Route path='/event' element={<HalamanEvent />} />
+        <Route path='/gallery-foto' element={<HalamanGalleryFoto />} />
+        <Route path='/klien-kami' element={<HalamanKlienKamu />} />
+        <Route path='/kontak-kami' element={<HalamanKontakKami />} />
+        <Route path='/profile' element={<HalamanProfile />} />
+        <Route path='/sign-in' element={<HalamanSignIn />} />
+        <Route path='/sign-up' element={<HalamanSignUp />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
