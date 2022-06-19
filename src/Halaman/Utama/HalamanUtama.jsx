a9c1627e1a0fcda467c4a7cf3ce113888cd66636
Kaka <kakalition@gmail.com>
2022-06-19 17:10:58 +0700

    Membuat layout halaman utama

--- src/Halaman/Utama/HalamanUtama.jsx
+++ src/Halaman/Utama/HalamanUtama.jsx
@@ -6,15 +6,15 @@ export default function HalamanUtama() {
   return (
     <div id="halaman-utama" className="h-screen w-screen flex flex-col">
       <div id="header" className="w-full flex flex-row">
-        <div className="w-28 h-28 p-4 flex items-center justify-center" >
+        <div className="w-32 h-32 p-4 flex items-center justify-center" >
           <img src={gear} />
         </div>
-        <div className="w-full h-28 pl-4 flex items-center" >
+        <div className="w-full h-32 pl-4 flex items-center" >
           <h1 className="font-libre-baskerville text-black text-5xl">Club Motor Agitryan</h1>
         </div>
       </div>
       <div id="navigasi-1" className="w-full h-12 flex flex-row" >
-        <div id="home" className='w-24 h-full flex items-center justify-center border-t-2 border-b-2 border-black'>
+        <div id="home" className='w-28 h-full flex items-center justify-center border-t-2 border-b-2 border-black'>
           <h2>Home</h2>
         </div>
         <div className='flex flex-row w-full'>
@@ -26,8 +26,17 @@ export default function HalamanUtama() {
         </div>
       </div>
       <div id="frame-konten-utama" className="w-full h-full flex flex-row" >
-        <div id="navigasi-2" className="w-24 h-full bg-yellow-600" />
-        <div id="konten-utama-2" className="w-full h-full bg-red-600" />
+        <div id="navigasi-2" className="w-28 h-full flex flex-col pl-2 gap-1" >
+          <div className='h-4' />
+          <h3>Artikel</h3>
+          <h3>Event</h3>
+          <h3>Gallery Foto</h3>
+          <h3>Klien Kami</h3>
+          <h3>Login</h3>
+          <h3 className="pl-4">Sign in</h3>
+          <h3 className="pl-4">Sign up</h3>
+        </div>
+        <div id="konten-utama-2" className="w-full h-full bg-gray-100 border-l-2 border-black " />
       </div>
     </div>
   );
