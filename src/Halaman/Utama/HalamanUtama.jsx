4e40f3de64f511d8dce2d4ec0a52cf2655e0010d
Kaka <kakalition@gmail.com>
2022-06-19 16:43:15 +0700

    Membuat layout kasar untuk halaman utama

--- src/Halaman/Utama/HalamanUtama.jsx
+++ src/Halaman/Utama/HalamanUtama.jsx
@@ -1,3 +1,15 @@
 export default function HalamanUtama() {
-  return ();
+  return (
+    <div id="halaman-utama" className="h-screen w-screen flex flex-col">
+      <div id="header" className="w-full flex flex-row">
+        <div className="w-28 h-28 bg-green-300" />
+        <div className="w-full h-28 bg-red-300" />
+      </div>
+      <div id="navigasi-1" className="w-full h-12 bg-sky-400" />
+      <div id="frame-konten-utama" className="w-full h-full flex flex-row" >
+        <div id="navigasi-2" className="w-24 h-full bg-yellow-600" />
+        <div id="konten-utama-2" className="w-full h-full bg-red-600" />
+      </div>
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import App from './App'
+import HalamanUtama from './Halaman/Utama/HalamanUtama'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} />
+        <Route path='/' element={<HalamanUtama />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
